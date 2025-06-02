fada91524d46abaa2df01da51acbef575bf86240
Ramon Asuncion <asuncionbatista@gmail.com>
2025-06-01 21:22:35 -0400

    Add winning game logic

--- board.cpp
+++ board.cpp
@@ -12,28 +12,93 @@ void board::init()
   grid = std::vector<std::vector<char>>(SIZE, std::vector<char>(SIZE, '-'));
 }
 
-void board::make_move(int col, int row, char symbol)
+bool board::make_move(int col, int row, char symbol)
 {
-  grid[row][col] = symbol;
+  if (is_empty(col, row)) {
+    grid[row][col] = symbol;
+    return true;
+  }
+  return false;
 }
 
 bool board::is_empty(int col, int row)
 {
-  return grid[col][row] == '-' ? true : false;
+  return grid[row][col] == '-';
+}
+
+bool board::is_draw() const
+{
+  for (int row = 0; row < SIZE; ++row) {
+    for (int col = 0; col < SIZE; ++col) {
+      if (grid[row][col] == '-') {
+        return false;
+      }
+    }
+  }
+  return true; // assumes check_winner is false.
+}
+
+bool board::check_winner(char player)
+{
+  // check cols
+  for (int row = 0; row < SIZE; ++row) {
+    bool row_win = true;
+    for (int col = 0; col < SIZE; ++col) {
+      if (grid[row][col] != player) {
+        row_win = false;
+        break;
+      }
+    }
+    if (row_win) return true;
+  }
+
+  // check rows
+  for (int col = 0; col < SIZE; ++col) {
+    bool col_win = true;
+    for (int row = 0; row < SIZE; ++row) {
+      if (grid[row][col] != player) {
+        col_win = false;
+        break;
+      }
+    }
+    if (col_win) return true;
+  }
+
+  // check diag
+  bool diag_win = true;
+  for (int i = 0; i < SIZE; ++i) {
+    if (grid[i][i] != player) {
+      diag_win = false;
+      break;
+    }
+  }
+  if (diag_win) return true;
+
+  // check anti diag
+  diag_win = true;
+  for (int i = 0; i < SIZE; ++i) {
+    if (grid[i][SIZE - 1 - i] != player) {
+      diag_win = false;
+      break;
+    }
+  }
+  if (diag_win) return true;
+
+  return false;
 }
 
 void board::display() const
 {
-//   a     b     c
-//      |     |
-//1  -  |  -  |  -
-// _____|_____|_____
-//      |     |
-//2  -  |  -  |  -
-// _____|_____|_____
-//      |     |
-//3  -  |  -  |  -
-//      |     |
+  //   a     b     c
+  //      |     |
+  //1  -  |  -  |  -
+  // _____|_____|_____
+  //      |     |
+  //2  -  |  -  |  -
+  // _____|_____|_____
+  //      |     |
+  //3  -  |  -  |  -
+  //      |     |
   std::cout << "\n    a     b     c\n";
   for (int i = 0; i < SIZE; ++i) {
     std::cout << "       |     |      \n";
