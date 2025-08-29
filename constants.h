fb9df9290c5c754fc9f0f15850688340e8297d76
Ramon Asuncion <ramon@b1t.dev>
2025-08-29 04:32:09 -0400

    Decouple player param from check_winner
    * Able to detect winner by board state not by specific player.
    * Use constants instead of specific character (no magic numbers).

---
+++ constants.h
@@ -0,0 +1,13 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+constexpr int SIZE = 3;
+constexpr char EMPTY_CELL = '-';
+constexpr char PLAYER_X = 'X';
+constexpr char PLAYER_O = 'O';
+constexpr int WIN_SCORE = 10;
+constexpr int LOSE_SCORE = -10;
+constexpr int TIE_SCORE = 0;
+
+#endif /* _CONSTANTS_H_ */
+
